e85fb4dc1c47f88107971c8cf2e5c6ac5c2a2715
Olga Pysarenko <ola.shevchuk@gmail.com>
2023-11-02 23:59:35 +0200

    done 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    comeIn(person: Person) {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log("Person has entered the house.");
+      } else {
+        console.log("The door is closed. Person cannot enter the house.");
+      }
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key) {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log("The door is opened.");
+      } else {
+        console.log("Invalid key. The door remains closed.");
+      }
+    }
+  }
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
